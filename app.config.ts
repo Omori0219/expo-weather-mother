2a5fdbe9b6f6aa8169197ade24393bc7eb619993
Omori0219 <oomoris0219@gmail.com>
2025-02-07 18:41:03 +0900

    「お天気かあさん」に名称変更

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: 'お天気おかん',
+  name: 'お天気かあさん',
   slug: 'weather-mother',
   version: '1.0.0',
   orientation: 'portrait',
